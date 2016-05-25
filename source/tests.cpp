2e3ff54ecd1ace9bd8810454ffb2de3106f8317d
DonEduardo27 <michael.schwarzkopf@uni-weimar.de>
2016-05-25 20:35:09 +0200

    Schonmal den Code aus der PDF reinkopiert, genug für heute. Btw, den ganzen glfw bums braucht man eigentlich nicht, trau mich aber nich das rauszunehmen, nich dasses dann nichmehr geht...

---
+++ source/tests.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <math.h>
+#include "List.hpp"
+TEST_CASE("describe_Vec2_Plus","[Vec2]")
+{
+int i;
+}
+
+int main(int argc, char *argv[])
+{
+  return Catch::Session().run(argc, argv);
+}
