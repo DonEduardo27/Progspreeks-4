d4130fd09af4221fed3876c3c7817d28e5a64266
DonEduardo27 <michael.schwarzkopf@uni-weimar.de>
2016-05-27 10:45:05 +0200

    2

--- source/List.hpp
+++ source/List.hpp
@@ -9,8 +9,12 @@ struct List ;
 template < typename T >
 struct ListNode
 {
-	ListNode(): m_value(), m_prev(nullptr), m_next(nullptr) {}ListNode(T const& v, ListNode *prev, ListNode *next )
-	: m_value ( v ) , m_prev ( prev ) , m_next ( next )
+	ListNode(): 
+	m_value(), m_prev(nullptr), m_next(nullptr) {}
+
+	ListNode(T const& v, ListNode *prev, ListNode *next ): 
+	m_value ( v ) , m_prev ( prev ) , m_next ( next )
+
 	{}
 	T m_value ;
 	ListNode * m_prev ;
@@ -47,8 +51,96 @@ public :
 List():
 m_size{0}, m_first {nullptr}, m_last {nullptr}{};
 
+bool empty () const 
+{
+	return m_size == 0;
+}
+
+std :: size_t size () const 
+{
+	return m_size;
+}
+
+void push_front(T const& val)
+{	
+	if(empty())
+	{
+		ListNode <T>* newNodeFirst = new ListNode<T>{val, nullptr, nullptr};
+
+		m_first = newNodeFirst;
+		m_last  = newNodeFirst;
+		
+	}
+	else
+	{
+		ListNode <T>* newNodeFirst = new ListNode<T>{val, nullptr, m_first};
+		 m_first = newNodeFirst;
+		(*m_first).m_prev = newNodeFirst;
+		
+	}
+	++m_size;
+}
 
 
+void push_back(T const & v)
+{	
+	if(empty())
+	{
+		ListNode <T>* newright= new ListNode<T>{v, nullptr, nullptr};
+		m_first = newright;
+		m_last  = newright;
+		
+	}else
+	{
+		ListNode <T>* newright= new ListNode<T>{v, m_last , nullptr};
+		 m_last=newright;
+		(*m_first).m_next=newright;
+	}
+	++m_size;
+}
+
+void pop_front()
+{
+	if(!empty())
+	{
+		
+		assert(m_first!=nullptr); // makro
+		delete m_first;
+		m_first = (*m_first).m_next;
+
+		--m_size;
+	}
+}
+void pop_back()
+{
+	if(!empty())
+	{
+		
+		assert(m_last!=nullptr); // makro
+		delete m_last;
+		m_first = (*m_first).m_next;
+		
+		--m_size;
+	}
+}
+
+
+T & front() const
+{
+	return((*m_first).m_value);
+}
+T & back() const
+{
+	return((*m_last).m_value);
+}
+/*void push_front() 
+{
+
+} 
+push_back() 
+
+front()
+back()*/
 
 
 	typedef T value_type ;
@@ -62,15 +154,7 @@ m_size{0}, m_first {nullptr}, m_last {nullptr}{};
 	friend class ListConstIterator <T >;// not implemented yet
 
 
-bool empty () const 
-{
-	return m_size == 0;
-}
-// http :// en . cppreference . com / w / cpp / container / list / size
-std :: size_t size () const 
-{
-	return m_size;
-}
+
 
 private :
 	std :: size_t m_size = 0;
--- source/tests.cpp
+++ source/tests.cpp
@@ -10,6 +10,28 @@ REQUIRE(liste.empty());
 REQUIRE(liste.size() == 0);
 }
 
+TEST_CASE("2","[two]")
+{
+List<int> liste {};
+
+liste.push_front (27);
+REQUIRE(liste.front() == 27);
+
+liste.pop_front();
+REQUIRE(liste.empty());
+
+liste.push_back(11);
+liste.push_back(12);
+
+REQUIRE(liste.front()==11);
+REQUIRE(liste.back()==12);
+
+liste.pop_back();
+REQUIRE(liste.size()==1);
+}
+
+
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
